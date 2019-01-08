c7f9e9de6510f408bee95f956364548bdbcfd8f8
Mike Portman <mikeportmanj@gmail.com>
2019-01-07 20:21:55 -0800

    init

---
+++ react_austin_weather/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class header from React.Component{
+    render(){
+        return(
+            <h1>WEEKLY AUSTIN WEATHER FORECAST</h1>
+        );
+    }
+}
+
+export default header;
