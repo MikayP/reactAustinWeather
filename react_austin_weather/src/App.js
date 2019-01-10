603343e4549172550fb8e505083e2560d067a935
Mike Portman <mikeportmanj@gmail.com>
2019-01-10 09:21:40 -0800

    compo

--- react_austin_weather/src/App.js
+++ react_austin_weather/src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import ReactDOM from 'react-dom';
+
+
+const API_Key = "583f803dfc6a7f2f96ff9957c330c2b0";
+
 
 class App extends Component {
+  getWeather = async () =>{
+    const API_Address = 'https://api.openweathermap.org/data/2.5/weather?q=Austin,USA&appid=${API_Key}';
+    const api_call = await fetch(API_Address);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <div className="App">
--- react_austin_weather/src/index.js
+++ react_austin_weather/src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './components/header';
 import * as serviceWorker from './serviceWorker';
+import App from 'App.js';
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+ReactDOM.render(
+<Header />,
+<App />, 
+    document.getElementById('root')
+    );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
