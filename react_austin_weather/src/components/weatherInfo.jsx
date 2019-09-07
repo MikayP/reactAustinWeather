88f4b7c277e15ffb19601a0126c45ebc9bd3ee2c
mikayp <mikeportmanj@gmail.com>
2019-09-06 19:51:45 -0500

    update for mentor

--- react_austin_weather/src/components/weatherInfo.jsx
+++ react_austin_weather/src/components/weatherInfo.jsx
@@ -6,57 +6,51 @@ import React from "react";
 //const REACT_APP_API_ADDRESS = `${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`;
             // console.log('HI', REACT_APP_OPEN_WEATHER_API_KEY);
 
-class WeatherInfo extends React.Component {
+            class WeatherInfo extends React.Component {
 
-        constructor() {
-            super();
-            this.state = {
-                items: [],
-                isLoaded: false,
-            }
-        }
-        componentDidMount() {
-            
-            fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Austin,USA&appid=1a94fd127ebfea3f96e4a60f9a349e5f&units=imperial`)
-                .then(results => results.json())
-                .then(json => {
-                       for (i = 0, i < items.list.length; i++){
-                           var date = items.list.dt_txt[i];
-
-                           if date.char
-                       }
-                        this.setState({
-                            isLoaded: true,
-                            items: json                           
-                        })
-                    });
-                    
+                constructor() {
+                    super();
+                    this.state = {
+                        items: [],
+                        isLoaded: false
+                    }
                 }
+         
+                componentDidMount() {
+                    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Austin,USA&appid=583f803dfc6a7f2f96ff9957c330c2b0&units=imperial`)
+                        .then(results => results.json())
+                        .then(json => {
+                                this.setState({
+                                    isLoaded: true,
+                                    items: json                           
+                                })
+                            });
+                        }
+        
+                    render() {
+        
+                        let {
+                            isLoaded,
+                            items
+                        } = this.state;
+                        if (!isLoaded) {
+                            return (<div> Loading... </div>)
+                        } else {
+                            return ( <div>
+                                <ul>
+                                    {items.list.map((item, key) => (
+                                        <li key="{key}">
+                                          {item.main.temp}
+                                        </li>
+                                    ))}
+                                </ul> 
+                                </div>
+                            );
+                        }
+                    }
+                } 
 
-            render() {
-       
-            
-                let {
-                    isLoaded,
-                    items
-                } = this.state;
-                if (!isLoaded) {
-                    return (<div> Loading... </div>)
-                } else {
-                    return ( <div>
-            
-                        <ul>
-                            {items.list.map((item, key) => (
-                                <li key={key}>
-                                    {item.main.temp}
-                                </li>
-                            ))}
-                        </ul> 
-                        </div>
-                    );
-                }
-            }
-        } 
+        
 
         export default WeatherInfo;
 
